acfe7e8bdd921eabd593b35fd431d1fe6d2ba8f2
thomasyu888 <thomasyu888@gmail.com>
2015-12-06 01:38:05 -0800

    Add global.R to add in global variables

---
+++ global.R
@@ -0,0 +1,21 @@
+library(synapseClient)
+library(shiny)
+library(plotly)
+synapseLogin()
+
+#vdsRdf <- synGet("syn5520037")
+
+#vdsRho <- synGet("syn5520030")
+
+
+#vdsRdf <- read.csv(vdsRdf@filePath)
+
+#vdsRho <- read.csv(vdsRho@filePath)
+#row.names(vdsRho) = vdsRho$X
+#vdsRho <- vdsRho[,-1]
+
+organ <- colnames(vdsRho)
+diseases <- levels(vdsRdf$disease)
+drugs <- levels(vdsRdf$drug)
+
+
