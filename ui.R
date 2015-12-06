559c78d08ccff2e8f7887119402654b3d7c2e32e
thomasyu888 <thomasyu888@gmail.com>
2015-12-06 01:39:16 -0800

    Add in sort rho values, and update way drop down menus are read

--- ui.R
+++ ui.R
@@ -1,6 +1,4 @@
-library(shiny)
-library(plotly)
-library(shiny)
+
 shinyUI(fluidPage(
   
   # Application title
@@ -12,16 +10,12 @@ shinyUI(fluidPage(
       sidebarLayout(
         sidebarPanel(
           selectInput("dataset", "Choose a dataset:",
-                      choices =list.files("out",full.names = T)),
+                      choices =drugs),
           
           selectInput("disease", "Choose an area:",
-                      choices = c("BLCA", "BRCA", "CESC","COAD", "ESCA", "GBM",  "HNSC", "KIRC", "KIRP", "LGG",  "LIHC",
-                                  "LUAD", "LUSC", "OV", "PAAD", "PCPG", "PRAD", "SARC", "SKCM",
-                                  "STAD", "TGCT", "THCA", "THYM")),
-          textOutput("drug")
+                      choices = diseases)
           
-         # selectInput("disease", "Choose an area:",
-          #            choices = names(R))
+
         ),
         
         # Show a plot of the generated distribution
@@ -30,13 +24,10 @@ shinyUI(fluidPage(
         )
       )
     ), # End Model 2 Tab Panel
-    tabPanel("Mdoel 1", 
+    tabPanel("Model 1", 
       selectInput("organ", "Choose an area:",
-                  choices = c("ALL", "autonomic_ganglia", "bone","breast", 
-                              "central_nervous_system", "endometrium",  "kidney", 
-                              "large_intestine", "liver", "lung",  "oesophagus",
-                              "ovary", "pancreas", "skin", "soft_tissue", "stomach", 
-                              "thyroid", "upper_aerodigestive_tract", "urinary_tract")),
+                  choices = organ),
+      checkboxInput("sort", "Sort Rho values", FALSE),
       plotlyOutput("vfsperf"))
   )
 ))
