13a9285e30968ccf08683c1c50f0f00f50c08b84
thomasyu888 <thomasyu888@gmail.com>
2015-12-12 22:45:31 -0800

    Round all numbers to 3 siginificant digits, make sure stringsAsFactors is False

--- global.R
+++ global.R
@@ -8,15 +8,18 @@ synapseLogin()
 #vdsRho <- synGet("syn5520030")
 
 
-#vdsRdf <- read.csv(vdsRdf@filePath)
+#vdsRdf <- read.csv(vdsRdf@filePath,stringsAsFactors = F)
 
-#vdsRho <- read.csv(vdsRho@filePath)
+#vdsRho <- read.csv(vdsRho@filePath,stringsAsFactors = F)
 #row.names(vdsRho) = vdsRho$X
 #vdsRho <- vdsRho[,-1]
 
 organs <- colnames(vdsRho)
-diseases <- levels(vdsRdf$disease)
-drugs <- levels(vdsRdf$drug)
-
-showtable <- c("genes","effect","effectSD","freqCounts","freqEvents","drug","disease")
+diseases <- unique(vdsRdf$disease)
+drugs <- unique(vdsRdf$drug)
+#Three significant Digits
+vdsRdf$effect <- signif(vdsRdf$effect,3)
+vdsRdf$freqEvents <- signif(vdsRdf$freqEvents,3)
+showtable <- c("genes","effect",#"effectSD",
+               "freqCounts","freqEvents","drug","disease")
 
