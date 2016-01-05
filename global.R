fcf734108c2f1c39c46c102781e338b7dd088f2c
xindiguo <xindi.guo@sagebase.org>
2016-01-05 14:21:00 -0800

    Initial loading of values, delete local read.csv

--- global.R
+++ global.R
@@ -3,20 +3,17 @@ library(shiny)
 library(plotly)
 synapseLogin()
 
-#vdsRdf <- synGet("syn5520037")
-
-#vdsRho <- synGet("syn5520030")
-
-
-#vdsRdf <- read.csv(vdsRdf@filePath,stringsAsFactors = F)
-
-#vdsRho <- read.csv(vdsRho@filePath,stringsAsFactors = F)
-
-#vdsRdf <- read.csv("shortened_vdsRdf.csv",stringsAsFactors = F)
-#vdsRho <- read.csv("vdsRhoEstimates.csv",stringsAsFactors = F)
-#drugRho <- read.csv("RdrugRho.tsv",stringsAsFactors = F,sep="\t")
-#row.names(vdsRho) = vdsRho$X
-#vdsRho <- vdsRho[,-1]
+# vdsRdf <- synGet("syn5520037")
+# 
+# vdsRho <- synGet("syn5520030")
+# 
+# 
+# vdsRdf <- read.csv(vdsRdf@filePath,stringsAsFactors = F)
+# 
+# vdsRho <- read.csv(vdsRho@filePath,stringsAsFactors = F)
+# 
+# row.names(vdsRho) = vdsRho$X
+# vdsRho <- vdsRho[,-1]
 
 organs <- colnames(vdsRho)
 diseases <- unique(vdsRdf$disease)
