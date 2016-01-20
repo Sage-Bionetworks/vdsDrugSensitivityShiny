35e50151ad5edd3294149b2f2e899753b748375c
thomasyu888 <thomasyu888@gmail.com>
2016-01-20 15:56:32 -0800

    Refresh

--- global.R
+++ global.R
@@ -1,22 +1,24 @@
+<<<<<<< HEAD
+=======
 library(synapseClient)
 library(shiny)
 library(plotly)
 synapseLogin()
 
-##R data frame file (Model 2)
 #vdsRdf <- synGet("syn5520037")
+
+#vdsRho <- synGet("syn5520030")
+
+
 #vdsRdf <- read.csv(vdsRdf@filePath,stringsAsFactors = F)
 
-##Rho values (Cell line drug sensitivity)
-#vdsRho <- synGet("syn5520030")
 #vdsRho <- read.csv(vdsRho@filePath,stringsAsFactors = F)
+
+#vdsRdf <- read.csv("shortened_vdsRdf.csv",stringsAsFactors = F)
+#vdsRho <- read.csv("vdsRhoEstimates.csv",stringsAsFactors = F)
+#drugRho <- read.csv("RdrugRho.tsv",stringsAsFactors = F,sep="\t")
 #row.names(vdsRho) = vdsRho$X
 #vdsRho <- vdsRho[,-1]
-
-##Disease drug sensitivity
-#drugRho <- synGet("syn5578061")
-#drugRho <- read.csv(drugRho@filePath, stringsAsFactors = F)
-
 
 organs <- colnames(vdsRho)
 diseases <- unique(vdsRdf$disease)
@@ -27,3 +29,4 @@ drugs <- sort(drugs)
 showtable <- c("genes","effect",#"effectSD",
                "freqCounts","freqEvents","drug","disease")
 
+>>>>>>> parent of e2bf8cd... Add commenting and add drugRho csv
